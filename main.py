fdfc2cc9f053926e2e1fffe53ca66f3bb56e8ec0
stumble_cat <pmwenger@gmail.com>
2025-08-03 15:02:29 -0500

    Update: 0.0.1 Baseline

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+from constants import *
+
+def main():
+    pygame.init()
+    print(
+        "Starting Asteroids!"
+        "Screen width: 1280"
+        "Screen height: 720"    
+    )
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    
+if __name__ == "__main__":
+    main()
